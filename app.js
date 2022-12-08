fd7de10018949c5c7cf1fa7f10445355628941ed
faldea1 <faldea1@uc.cl>
2022-12-08 23:04:09 +0000

    random functions ready

--- app.js
+++ app.js
@@ -1,6 +1,32 @@
-//PRIMER EVENTO EN EL CICLO DE SITIO WEB-> ONLOAD. APP/CODE COMIENZA A CORRER.
+//PRIMER EVENTO EN EL CICLO DE SITIO WEB-> ONLOAD. APP/CODE COMIENZA A CORRER:
 
-window.onload = function (){
-    console.log('verificando funcionamiento...')
+window.onload = function () {
+    console.log('verificando funcionamiento...');
+
+}
+
+
+
+
+//FUNCIONES PARA MOSTRAR CARTA AL AZAR (N° y PALO):
+
+
+let RandomCardNumber = () => {
+
+    let numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+    let numberposition = Math.floor(Math.random() * numbers.length);
+
+    return numbers[numberposition];
+
+}
+
+let RandomCardSuit = () => {
+
+    let suits = ["♣", "♠", "♦ ", "♥"];
+
+    let suitsposition = Math.floor(Math.random() * suits.length);
+
+    return suits[suitsposition];
 
 }
